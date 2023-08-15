61d71eb415492d7db2dd6467722dc8b70f9dc9f0
ANSHA-KHAN <142288669+ANSHA-KHAN@users.noreply.github.com>
2023-08-15 17:55:59 +0500

    ANSHA.py

--- ANSHA.py
+++ ANSHA.py
@@ -1,6 +1,6 @@
 import os, platform, time, sys
 os.system('git pull')
-print('\033[1;37m Tools Is On Updating Please wait...\033[0;m')
+print('\033[1;32m Tools Is On Updating Please wait...\033[0;m')
 exit()
 bit = platform.architecture()[0]
 if bit == '64bit':
